49999385e2b80a91440c53a274b200c294d9a5b3
Maiseyeva Tatsiana <maiseyeva.tatsiana@gmail.com>
2023-09-24 21:25:55 +0300

    homework 3 task 7 improvements

--- Homework_3/task_7.py
+++ Homework_3/task_7.py
@@ -1,11 +1,18 @@
+"""
+Определить является ли число простым
+"""
+
 number = int(input("Введите число: "))
 
 result = True
 
 if number > 1:
     for i in range(2, number):
-        divisor = number % i == 0
-        if divisor:
+        remainder = number % i == 0  # проверка, что число является делитель
+        if remainder:
             result = False
+            break
+else:
+    result = False
 
 print(result)
