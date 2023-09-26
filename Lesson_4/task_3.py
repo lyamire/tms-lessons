fd1ae85ce1317d0afffc4be6e2cd484ffb9df106
Maiseyeva Tatsiana <maiseyeva.tatsiana@gmail.com>
2023-09-26 20:57:51 +0300

    add Lesson 4 change task 3

--- Lesson_4/task_3.py
+++ Lesson_4/task_3.py
@@ -1,8 +1,11 @@
 import random
 number = random.randint(1, 5)
-entered_number = int(input("Введите число от 1 до 5: "))
-if entered_number == number:
-    print("Поздравляю, Вы угадали число")
-else:
-    print("К сожалению, Вы не угадали")
+
+solved = False
+while not solved:
+    entered_number = int(input("Введите число от 1 до 5: "))
+    if entered_number == number:
+        print("Поздравляю, Вы угадали число")
+    else:
+        print("Вы не угадали, попробуйте еще раз ")
 
