30a85810cf9e11647246c925ac59ee715a2050d2
Maiseyeva Tatsiana <maiseyeva.tatsiana@gmail.com>
2023-09-26 21:46:28 +0300

    add Lesson 4 task 3

--- Lesson_4/task_3.py
+++ Lesson_4/task_3.py
@@ -1,12 +1,11 @@
 import random
 number = random.randint(1, 5)
 
-solved = False
-while not solved:
+while True:
     entered_number = int(input("Введите число от 1 до 5: "))
     if entered_number == number:
         print("Поздравляю, Вы угадали число")
-        solved = True
+        break
     else:
         print("Вы не угадали, попробуйте еще раз ")
 
