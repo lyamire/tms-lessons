eec1d47d5fd6f7b5e2bacecca26a924f423a1645
Maiseyeva Tatsiana <maiseyeva.tatsiana@gmail.com>
2023-09-30 15:34:14 +0300

    Add homework 4

--- Homework_04/task_1.py
+++ Homework_04/task_1.py
@@ -1,14 +1,27 @@
-# variant 1
+"""
+Вывести на экран числа кратные 5 от 0 до 100 включительно.
+"""
+
+# variant 1 Сделать это с помощью функции range с шагом 5
+
+
 sum_numbers_1 = 0
 for i in range(0, 101, 5):
     sum_numbers_1 += i
 
-print(sum_numbers_1)
 
-# variant 2
+assert sum_numbers_1 == 1050
+assert sum_numbers_1 != 1055
+
+
+# variant 2 Сделать это с помощью функции range c шагом 1 и вложенным if
+
+
 sum_numbers_2 = 0
 for i in range(0, 101):
     if i % 5 == 0:
         sum_numbers_2 += i
 
-print(sum_numbers_2)
+
+assert sum_numbers_2 == 1050
+assert sum_numbers_2 != 1150
