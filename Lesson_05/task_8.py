9486793b873f805516ce4a007547bd9f586f5463
Maiseyeva Tatsiana <maiseyeva.tatsiana@gmail.com>
2023-10-01 15:23:00 +0300

    Add lesson 5

--- Lesson_05/task_8.py
+++ Lesson_05/task_8.py
@@ -3,12 +3,26 @@
 Использование самой функции round запрещено.
 """
 
-def my_round(number: float, digits: int):
-    delimiter = 0.1 ** digits
-    result_int = number // delimiter
-    result_int = result_int * delimiter
-    return float(f"{result_int:.10f}")
+def my_round(number: float, digits: int = 0):
+    multiplier = 10 ** digits
+    diff = 0.5
+    if number < 0:
+        diff = -0.5
+    round_number = int(number * multiplier + diff) / multiplier
+    return round_number
 
 
-assert my_round(number=3.443534, digits=3) == 3.443
-assert my_round(number=3.443534, digits=3) != 2.44
+assert my_round(number=3.443534, digits=3) == 3.444
+assert my_round(123.456, 3) == 123.456
+assert my_round(123.456, 2) == 123.46
+assert my_round(123.456, 1) == 123.5
+assert my_round(123.456) == 123.0
+assert my_round(123.456, -1) == 120.0
+assert my_round(123.456, -2) == 100.0
+assert my_round(123.456, -3) == 0.0
+assert my_round(3.443534, 1) == 3.4
+assert my_round(123.567, 2) == 123.57
+assert my_round(123.890, 2) == 123.89
+assert my_round(-123.456, 2) == -123.46
+assert my_round(1.5) == 2
+assert my_round(2.5) == 3
