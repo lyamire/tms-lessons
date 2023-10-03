87d59d2d85b04c5788b532544429703790ab7df2
Maiseyeva Tatsiana <maiseyeva.tatsiana@gmail.com>
2023-10-03 15:12:54 +0300

    Fix for hw4, task 1

--- Homework_04/task_1.py
+++ Homework_04/task_1.py
@@ -4,24 +4,9 @@
 
 # variant 1 Сделать это с помощью функции range с шагом 5
 
-
-sum_numbers_1 = 0
-for i in range(0, 101, 5):
-    sum_numbers_1 += i
-
-
-assert sum_numbers_1 == 1050
-assert sum_numbers_1 != 1055
-
+print(*[i for i in range(0, 101, 5)])
 
 # variant 2 Сделать это с помощью функции range c шагом 1 и вложенным if
 
+print(*[i for i in range(0, 101) if i % 5 == 0])
 
-sum_numbers_2 = 0
-for i in range(0, 101):
-    if i % 5 == 0:
-        sum_numbers_2 += i
-
-
-assert sum_numbers_2 == 1050
-assert sum_numbers_2 != 1150
