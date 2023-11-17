aff2aed6b825c50dde783f2f2a90f9c8ee6d5adc
Maiseyeva Tatsiana <maiseyeva.tatsiana@gmail.com>
2023-11-17 09:09:10 +0300

    fix task 3 homework 12

--- Homework_12/task_03.py
+++ Homework_12/task_03.py
@@ -1,14 +1,14 @@
 import re
 
 def is_float_number(stroke: str) -> bool:
-    regex = r'\d+\.\d+'
+    regex = r'-?\d+\.\d+'
     return re.fullmatch(regex, stroke) is not None
 
 
 if __name__ == '__main__':
     assert is_float_number('1.0')
     assert is_float_number('3.14')
-    assert is_float_number('2345436456457754745754574.23423423423423424')
+    assert is_float_number('-2345436456457754745754574.23423423423423424')
     assert not is_float_number('1111')
     assert not is_float_number('1122.')
     assert not is_float_number('232423,34234')
